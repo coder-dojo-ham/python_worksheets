3ca0fdcfb6c32802f83a59209b468e88af3cddbf
Nick <nick.a.sarbicki@gmail.com>
2016-01-13 22:01:39 +0000

    Added comments to age1.py

--- Beginner/age1.py
+++ Beginner/age1.py
@@ -1,7 +1,13 @@
+# We need to store our users name in a variable.
+# to do this we use the input function
 name = input('What is your name? ')
 
+# You can concatenate strings together using the + operator
 print('Hi ' + name + ' I am Python!')
 
+# Input always assumes what the user writes is a string.
+# As a result we have to manually convert it to an integer
+# using the int function.
 years = int(input('How old are you ' + name  + '? '))
 
 months = years * 12
