25387ec2fb34ed11ffd0bc2c3900ae490df39972
Nick <nick.a.sarbicki@gmail.com>
2016-01-13 22:02:33 +0000

    Added comments to age2.py

--- Beginner/age2.py
+++ Beginner/age2.py
@@ -6,6 +6,8 @@ years = int(input('How old are you ' + name  + '? '))
 
 months = years * 12
 
+# If statements let us test the programs current state.
+# By testing this we can decide what to do next.
 if years > 18:
     print(name + ' did you know that you are ' + str(months) + ' months old? That is pretty old!')
 elif years < 4:
