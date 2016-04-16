09217a810bdfb08b8c45d0ebd4ee7bc4f042512e
nick.a.sarbicki@gmail.com <Nick.A.Sarbicki@gmail.com>
2016-04-16 12:06:54 +0100

    Adding in a template to give hints.

---
+++ Advanced/hangman_template.py
@@ -0,0 +1,30 @@
+"""
+This is a template for hangman, use the hints here to write your own program.
+"""
+
+import  # import a module which does random things here
+
+list_of_words = ['put', 'some', 'words', 'here']  # put your own words in here.
+
+word = some random word # choose a random word from your list, use the import above.
+
+# You might need to make some variables here to say how many tries has had, and their guesses.
+
+while some conditions:  # You need to loop around asking them to guess until they get it right or run out of tries.
+    print the hidden word  # you need a hidden version of the word to show how big it is and what they've guessed.
+    guess = a users guess# Ask them to guess a letter or word here.
+
+    if some guess is good: # check here if they got the right letter or word.
+        tell the user it is good.  # tell the user is good
+    else:  # make sure to tell them if it is wrong.
+        tell the user it is wrong
+
+    # outside of the if now.
+    add the guess to the list of guesses.
+
+# outside the loop now.
+
+if they won:
+    # tell them they won.
+else:
+    # tell them they lost.
