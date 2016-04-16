750ef478e833d84cbe9baa31295d7a8fc4d3388e
nick.a.sarbicki@gmail.com <Nick.A.Sarbicki@gmail.com>
2016-04-16 11:37:55 +0100

    Hangman is working. Needs doc.

--- Advanced/hangman.py
+++ Advanced/hangman.py
@@ -4,9 +4,10 @@ A refresh lesson to go over most of the control flow in Python and a few of the 
 
 import random
 
-words = []
+words = ['coder dojo', 'kingston', 'richmond', 'raspberry pi', 'python', 'ruby', 'computer', 'jurassic world',
+         'turtle', 'hangman']
 
-word = random.choice(words)
+word = random.choice(words).lower()
 
 revealed = ['_' if char != ' ' else char for char in word]
 
@@ -16,12 +17,15 @@ tries = 0
 
 while ''.join(revealed) != word and tries < 15:
     print(''.join(revealed))
-    guess = input('Which letter do you want to try?')
+    guess = input('\nWhich letter do you want to try? ').lower()
+
+    if guess == word:
+        break
 
     if guess not in guesses:
         guesses.append(guess)
         if guess in word:
-            positions = [i for i, chr in enumerate(word) if chr == guess]
+            positions = [i for i, char in enumerate(word) if char == guess]
             for pos in positions:
                 revealed[pos] = guess
             print('Yes that was in the word!')
@@ -32,9 +36,9 @@ while ''.join(revealed) != word and tries < 15:
         print('You guessed that already!')
     tries += 1
 
-    print('You have had {} tr{}'.format(tries, 'ies' if tries > 1 else 'y'))
+    print('You have had {} tr{} out of 15'.format(tries, 'ies' if tries > 1 else 'y'))
 
-if guess == word:
+if word in (guess, ''.join(revealed)):
     print('Congrats! You won!')
 else:
     print('Sorry you lost :-(')
