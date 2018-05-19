f15d1101a5df9627fb7336238974d99f4ef93bbb
Nick Sarbicki <nick.a.sarbicki@gmail.com>
2018-05-19 14:27:32 +0100

    Fix randint bug in part 6

--- Intermediate/part_06/food_generator.py
+++ Intermediate/part_06/food_generator.py
@@ -4,8 +4,8 @@ food = ['Macaroni', 'Chicken', 'Spaghetti', 'Fish', 'Eggs', 'Sausage', 'Noodles'
 
 toppings = ['Cheese', 'Rice', 'Meatballs', 'Chips', 'Toast', 'Mash', 'Salad', 'Ice Cream', 'Quiche', 'Milk']
 
-random_food = food[random.randint(len(food))]
+random_food = food[random.randint(0, len(food))]
 
-random_topping = toppings[random.randint(len(toppings))]
+random_topping = toppings[random.randint(0, len(toppings))]
 
 print('For today dinner is ' + random_food + ' and ' + random_topping + '!')
